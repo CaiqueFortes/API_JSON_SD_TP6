ca5ca497a7cead7c1d551604f35c3f478ee32976
caihumm <fortescaique@gmail.com>
2024-04-23 13:37:03 -0300

    [FEAT] Alterei o programa para cadastrar e ler livros.

---
+++ API_JSON_SD_TP6/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API_JSON_SD_TP6.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> _books = new List<Book>();
+
+        [HttpGet]
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _books;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            if (book == null)
+                return BadRequest();
+
+            book.Id = _books.Count + 1; // Simplesmente incrementa o ID para simulação
+            _books.Add(book);
+
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+    }
+}
