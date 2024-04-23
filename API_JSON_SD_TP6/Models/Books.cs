aeec493aecc9d223b407b7ba001c9457ee06da62
caihumm <fortescaique@gmail.com>
2024-04-23 15:46:30 -0300

    [FIX] Comentario e exclusão de pasta não usada

--- API_JSON_SD_TP6/Models/Books.cs
+++ API_JSON_SD_TP6/Models/Books.cs
@@ -3,7 +3,7 @@
     public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
-    public string ISBN { get; set; }
+    public string ISBN { get; set; } // International Standard Book Number -> Número Padrão Internacional de Livro
     public string Publisher { get; set; }
     public int Year { get; set; }
 }
